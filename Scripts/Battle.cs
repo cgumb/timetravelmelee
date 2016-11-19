bb4ad2a390194562985ee91fd564a1fcce413929
scatophage <chrisgumb@gmail.com>
2016-11-19 12:08:18 -0500

    removed extra Scripts directory

--- Scripts/Battle.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic; // for List
-
-// Used by Battle State Machine to load specific units for a given battle
-public class Battle : MonoBehaviour
-{
-	public List<GameObject> charactersInBattle; // list of heroes and enemies in the battle
-}
